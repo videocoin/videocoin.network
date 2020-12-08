726f0a691a0b47a6deca0ddf78c0b393ce742ec3
Alexander Polovnikov <aleksandrpol@gmail.com>
2020-12-09 02:02:27 +0300

    fix footer typo

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -18,7 +18,7 @@ const Footer = () => {
             <Link href="https://forum.videocoin.network/">Community Forum</Link>
             <Link href="mailto:support@videocoin.network">Contact Support</Link>
             <RouteLink to="download-hub">Downloads</RouteLink>
-            <Link href="https://github.com/videocoin">Githun</Link>
+            <Link href="https://github.com/videocoin">Github</Link>
             <Link href="https://videocoin.io">Token</Link>
             <Link href="https://liveplanet.net">About Live Planet</Link>
           </Links>
